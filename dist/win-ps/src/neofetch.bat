6c29b17204fa16260a9dabea3bf9efdccf15bff4
Steve <149971541+ShadowPlayzDev@users.noreply.github.com>
2026-02-22 13:28:25 -0600

    typo

--- dist/win-ps/src/neofetch.bat
+++ dist/win-ps/src/neofetch.bat
@@ -2,5 +2,5 @@
 :: Batch relys on PowerShell as a dependancy
 setlocal
 set "WRKDIR=%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%WRKDIR%fetch.ps1" %*
+powershell -NoProfile -ExecutionPolicy Bypass -File "%WRKDIR%neofetch.ps1" %*
 endlocal
